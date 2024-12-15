10d4f77ad8c8d7b3bdda6bb8e25996dbd82f6e59
Dinesh S <dineshindevelope@gmail.com>
2024-12-15 09:37:40 +0530

    card section working

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -44,7 +44,74 @@ const Main = () => {
               the Best Solutions to Overcome Every Obstacle.
             </h1>
           </div>
-          <div></div>
+          <div className="pt-20 grid grid-cols-5">
+            <div className="relative w-56 h-auto group">
+              <img
+                src="https://www.houseofmarktech.com/assets/playingcard-BCNbyrsW.png"
+                alt="card-logo"
+                className="w-full h-full object-cover"
+              />
+              <div className="absolute top-0 left-0 w-full h-full opacity-0 group-hover:opacity-100 transition-opacity duration-300">
+                <div className="w-full h-full border border-gray-300 rounded-xl overflow-hidden">
+                  <div className="bg-black h-40">
+                    <p className="font-bold text-xl pl-7 pt-5 text-white">
+                      Lack of <br /> Innovation in <br /> Growth?
+                    </p>
+                  </div>
+                  <div className="bg-white h-40">
+                    <p className="font-bold text-2xl pl-5 pt-3 text-black">
+                      We drive Growth through tech, data, and strategy.
+                    </p>
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div className="relative w-56 h-auto group">
+              <img
+                src="https://www.houseofmarktech.com/assets/playingcard-BCNbyrsW.png"
+                alt="card-logo"
+                className="w-full h-full object-cover"
+              />
+              <div className="absolute top-0 left-0 w-full h-full opacity-0 group-hover:opacity-100 transition-opacity duration-300">
+                <div className="w-full h-full border border-gray-300 rounded-xl overflow-hidden">
+                  <div className="bg-black h-40">
+                    <p className="font-bold text-xl pl-7 pt-5 text-white">
+                      weak Brand <br /> Identity?
+                    </p>
+                  </div>
+                  <div className="bg-white h-40">
+                    <p className="font-bold text-2xl pl-5 pt-3 text-black">
+                      We craft bold <br /> Branding with <br /> standout <br />{" "}
+                      content.
+                    </p>
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div className="relative w-56 h-auto group ">
+              <img
+                src="https://www.houseofmarktech.com/assets/playingcard-BCNbyrsW.png"
+                alt="card-logo"
+                className="w-full h-full object-cover"
+              />
+              <div className="absolute top-0 left-0 w-full h-full opacity-0 group-hover:opacity-100 transition-opacity duration-300">
+                <div className="w-full h-full border border-gray-300 rounded-xl overflow-hidden">
+                  <div className="bg-black h-40">
+                    <p className="font-bold text-xl pl-7 pt-5 text-white">
+                      Inefficient <br /> Operations?
+                    </p>
+                  </div>
+                  <div className="bg-white h-40">
+                    <p className="font-bold text-2xl pl-5 pt-3 text-black">
+                      We optimize <br />
+                      processes with <br />
+                      streamlined <br /> Strategies.
+                    </p>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
         </div>
       </div>
     </div>
